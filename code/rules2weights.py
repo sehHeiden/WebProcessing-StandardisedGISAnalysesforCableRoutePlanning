ed0d4f79460b596b52f34349c65f46b4cef7bf93
Sebastian Heiden <tetje@yandex.ru>
2022-11-06 13:05:23 +0100

    correct the errors in the layer_weights.ods  describing the weights, chang
    rules2weights.py:
     - bugfix in write_compress: extracting the raster dtype
     - __filter (the vectors): now uses the LISPS syntax in the rules
     - filter_gdf: bugfix of parameter given to __filter
     - buffer of the vector: now uses the LISP like syntax.
     - process_default_rule renamed to process_base_rule, now returning the raster itself instead of the string
     - process_rule: bugfix in the return value str-> list[str],
       - more complete save file names
       - new rasterization parameters
       - return raster no has nodata values, set where the base raster is it set, too.

--- code/rules2weights.py
+++ code/rules2weights.py
@@ -10,77 +10,102 @@ from brackettree.nodes import TextNode, RoundNode
 from geocube.api.core import make_geocube
 from geocube.rasterize import rasterize_image
 from geopandas import read_file, GeoDataFrame
-from pandas import read_excel, DataFrame, concat, merge
+from pandas import read_excel, DataFrame, concat, merge, Series
 from rioxarray import open_rasterio
 from rioxarray.merge import merge_arrays
+from tqdm import tqdm
+from xarray import DataArray
 
 
-def write_compress(r, save: Path) -> None:
-    predict = 3 if r.dtype == 'float' else 2
-    r.rio.to_raster(save, tiled=True, compress='ZSTD', num_threads='ALL_CPUS', predictor=predict, zstd_level=1)
+def write_compress(r: DataArray, save: Path) -> None:
+    predict = 3 if np.issubdtype(r.dtype, np.floating) else 2
+    r.rio.to_raster(save, driver='GTiff', tiled=True, compress='ZSTD',
+                    num_threads='ALL_CPUS', predictor=predict, zstd_level=1)
 
 
 def __filter(total_gdf: GeoDataFrame, column: str, _rule: Node, ) -> GeoDataFrame:
+    child_results: list[GeoDataFrame] = []
+    for item in _rule.items:
+        if isinstance(item, RoundNode):
+            child_results.append(__filter(total_gdf, column, item))
 
-    child_results = concat([__filter(total_gdf, column, item) for item in rule.items if isinstance(item, RoundNode)])
+    if len(child_results) > 1:
+        child_result = concat(child_results)
+    elif len(child_results) == 1:
+        child_result = child_results[0]
+    else:
+        child_result = GeoDataFrame(data=[], columns=total_gdf.columns, crs=total_gdf.crs)
 
-    command = str(_rule).split(' ')
-    operator = command[0]
+    operator, identifiers = get_command(_rule)
 
     if operator == 'NOT':
-        return total_gdf - child_results
-    elif operator == 'OR':
-        if len(command) > 1:
-            new_results = total_gdf[total_gdf[column].isin(*command[1:])]
+        if len(identifiers) == 0:
+            return total_gdf[~total_gdf.index.isin(child_result.index)]
         else:
-            new_results = [total_gdf[total_gdf[column]] == item for item in _rule.items if isinstance(item, TextNode)]
-        return concat[child_results, new_results]
+            return total_gdf[~(total_gdf[column] == identifiers[0])]
+
+    elif operator == 'OR':
+        if len(identifiers) > 0:
+            new_results = total_gdf[total_gdf[column].isin(identifiers)]
+            return concat([child_result, new_results])
+        else:
+            return child_result
 
     elif operator == 'STARTSWITH':
-        return total_gdf[total_gdf[column].apply(lambda x: x.startswith(command[1]))]
+        return total_gdf[total_gdf[column].apply(lambda x: isinstance(x, str) and x.startswith(identifiers[0]))]
+    else:
+        return child_result
 
 
 def filter_gdf(_gdf: GeoDataFrame, column: str, values: str):
     # decompose the value
     start_node = Node(values)
-    return __filter(_gdf, column, values)
+    return __filter(_gdf, column, start_node)
 
 
 def buffer(_gdf: GeoDataFrame, distance: float | str) -> GeoDataFrame:
     _gdf = _gdf.copy()
 
-    if isinstance(distance, str) and 'COL:' in distance:
-        buffer_start = distance.find('COL:')
-        col_name = distance[buffer_start + 4:].split(' ', 1)[0]
-        buffer_end = buffer_start + len(col_name)
-        buffer_value = val if ~ (val := _gdf[col_name]).isna() else 0
+    if isinstance(distance, str):
+        distance = __estimate_buffer_size(_gdf, Series(np.zeros(len(_gdf)), index=_gdf.index), Node(distance))
+    else:
+        distance = distance if not np.isnan(distance) else 0
 
-        distance = distance[:buffer_start] + str(buffer_value) + distance[buffer_end:]
-
-    if isinstance(distance, str) and distance.startswith('MAX'):
-        distance = distance[4:-1]
-        distance = [float(x) for x in distance.split(' OR ')]
-        distance = np.max(distance)
-
-    _gdf['geometry'] = _gdf['geometry'].buffer(distance if not np.isnan(distance) else 0)
+    _gdf['geometry'] = _gdf['geometry'].buffer(distance)
     return _gdf
 
 
-def _split_col(split_str: str | float | int):
-    if isinstance(split_str, str):
-        if ' OR ' in split_str and ' OR ' in Node(split_str).items[0]:  # if OR and it's outside the brackets
+def __estimate_buffer_size(data: GeoDataFrame, _buffer: Series, _rule: Node, ) -> Series:
+    child_results: list[Series | np.ndarray] = []
+    for item in _rule.items:
+        if isinstance(item, RoundNode):
+            child_results.append(__estimate_buffer_size(data, _buffer, item))
 
-            split_str = split_str.split(' OR ')
-            if np.all([x.isdigit() for x in split_str]):
-                split_str = [float(x) for x in split_str]
-            return split_str
-        else:
-            return [split_str]
-    elif isinstance(split_str, float) and np.isnan(split_str):
-        return []
-
+    operator, identifiers = get_command(_rule)
+    if operator == 'MAX':
+        return Series(np.max([*[x.values for x in child_results],
+                              *[np.full(len(data), float(x)) for x in identifiers if x.isdigit()]],
+                             axis=0),
+                      index=data.index)
+    elif operator == 'COL':
+        return data[identifiers[0]].apply(lambda x: x if isinstance(x, float) and ~ np.isnan(x) and x != -9998 else 0)
+    elif operator == 'RoundNode':
+        return concat(child_results)
     else:
-        return [split_str]
+        raise NotImplementedError
+
+
+def get_command(_rule):
+    command = str(_rule.items[0]).split(' ')
+    command = [x.strip() for x in command]
+    operator = command[0]
+    identifiers = [x for x in command[1:] if len(x) > 0]
+    for i in _rule.items[1:]:
+        if isinstance(i, TextNode):
+            new_identifier = str(i).strip()
+            if len(new_identifier) > 0:
+                identifiers.append(new_identifier)
+    return operator, identifiers
 
 
 def make_processable(_df: DataFrame, main_path: Path) -> DataFrame:
@@ -97,26 +122,40 @@ def make_processable(_df: DataFrame, main_path: Path) -> DataFrame:
     return _df.loc[:, ['LineNum', 'Path', 'Description', 'Layer', 'Level', 'Column', 'ColumnValue', 'Buffer']]
 
 
-def process_default_rule(df: DataFrame, resolution: float | tuple[float, list], crs: int,
-                         _save_dir: Path, _all_touched: bool) -> list[str | Path]:
-    named_tuple = list((df.copy().loc[df['Description'] == 'Base']).itertuples())[0]
-    return process_rule(named_tuple, resolution, crs, _save_dir, _all_touched)
-
-
-def process_rule(named_tuple, resolution: float | tuple[float, list], crs: int,
-                 _save_dir: Path, _all_touched: bool) -> list[str | Path]:
-    gdf = read_file(named_tuple.Path) if np.isnan(named_tuple.Layer) else read_file(named_tuple.Path,
-                                                                                    layer=named_tuple.Layer)
-    gdf = gdf.to_crs(epsg=crs)
+def __vector_processing(named_tuple, _crs: int, base_extent: tuple | None = None) -> GeoDataFrame:
+    layer_name = named_tuple.Layer if isinstance(named_tuple.Layer, str) else None
+    gdf: GeoDataFrame = read_file(named_tuple.Path, bbox=base_extent, layer=layer_name)
+    gdf = gdf.to_crs(epsg=_crs)
+    gdf['Weights'] = named_tuple.Weight
 
     if isinstance(named_tuple.Column, str):
         gdf = filter_gdf(gdf, named_tuple.Column, named_tuple.ColumnValue)
-    gdf = buffer(gdf, named_tuple.Buffer)
+    return buffer(gdf, named_tuple.Buffer)
 
-    save_path = _save_dir / named_tuple.Path
-    raster = make_geocube(gdf, resolution=resolution, rasterize_function=partial(rasterize_image, _all_touched))
+
+def process_base_rule(df: DataFrame, _resolution: float | tuple[float, list], _crs: int,
+                      _save_dir: Path, _all_touched: bool) -> DataArray:
+    named_tuple = list((df.copy().loc[df['Description'] == 'Base']).itertuples())[0]
+    vec = __vector_processing(named_tuple, _crs)
+    save_path = _save_dir / f"rule_{named_tuple.LineNum}_{named_tuple.Description}.tif"
+    raster = make_geocube(vec, ['Weights', ], resolution=resolution, fill=999,
+                          rasterize_function=partial(rasterize_image, all_touched=_all_touched))
+    raster = raster['Weights']
     write_compress(raster, save_path)
-    return save_path
+    return raster
+
+
+def process_rule(named_tuple, example_da: DataArray, _crs: int,
+                 _save_dir: Path, _all_touched: bool, bbox) -> list[str | Path]:
+    vec = __vector_processing(named_tuple, _crs, bbox)
+
+    save_path = _save_dir / f"rule_{named_tuple.LineNum}_{named_tuple.Description}.tif"
+    raster = make_geocube(vec, ['Weights'], like=example_da, fill=999,
+                          rasterize_function=partial(rasterize_image, all_touched=_all_touched))
+    raster = raster['Weights']
+    raster.data[example_da == example_da.rio.nodata] = raster.rio.nodata
+    write_compress(raster, save_path)
+    return [save_path, ]
 
 
 if __name__ == '__main__':
@@ -124,11 +163,11 @@ if __name__ == '__main__':
     parser.add_argument('config_file_path', type=str, help='Full path to the .xlsx config file.')
     parser.add_argument('vectors_main_folder', type=str, help='Main Path to the vector files.')
     parser.add_argument('save_dir', type=str, help='Path where this projects raster files can the saved')
-    parser.add_argument('resolution', type=int, default=1000, help='Resolution to use.')
-    parser.add_argument('all_touched', type=bool, default=False, help='Select pixel only that`s pixel center  is '
-                                                                      'covered by the Polygone (False) or any point'
-                                                                      ' of the pixel overlaps with the Polygon (True).')
-    parser.add_argument('crs', type=int, default=3157, help='Reference system. Default: Google Pseudo Mercator.')
+    parser.add_argument('-r', '--resolution', type=int, default=1000, help='Resolution to use.')
+    parser.add_argument('-at', '--all_touched', action='store_true',  help='Select those pixels, those CENTER  is '
+                                                                           'covered by the Polygone (False) or any '
+                                                                           'part overlaps with the Polygone (True).')
+    parser.add_argument('--crs', type=int, default=3157, help='Reference system. Default: Google Pseudo Mercator.')
 
     args = parser.parse_args()
 
@@ -137,19 +176,22 @@ if __name__ == '__main__':
     weights = read_excel(args.config_file_path, sheet_name='Weights')
     rules = make_processable(rules, Path(args.vectors_main_folder))
     rules = merge(rules, weights, on='Level').drop(columns='Level')
+    resolution, crs, all_touched = args.resolution, args.crs, args.all_touched
 
     save_dir = Path(args.save_dir)
-    default_save = process_default_rule(rules, args.resolution, args.crs, save_dir, args.all_touched)
-    rules = rules.loc[~rules['Description'] == 'Basis']
+    base_raster = process_base_rule(rules, resolution, crs, save_dir, all_touched)
+    rules = rules.loc[~(rules['Description'] == 'Base')]
 
-    processed_rules = []
-    for rule in rules.iterrows():
-        processed_rules = processed_rules + process_rule(rule, args.resolution, args.crs, save_dir, args.all_touched)
+    finalized_rules = []
+    p_bar = tqdm(rules.itertuples(), total=len(rules))
+    for rule in p_bar:
+        p_bar.set_description(f"Rule {rule.LineNum}: {rule.Description}.")
+        finalized_rules = finalized_rules + process_rule(rule, base_raster, crs, save_dir, all_touched,
+                                                         base_raster.rio.bounds())
 
     # combine the rasters file
-    # processed_rules -> max, processed_rules + default (where processed is nodata)
-    processed_rules = merge_arrays([open_rasterio(x, cache=False) for x in processed_rules], method='max')
-    default_rules = open_rasterio(default_save)
+    # finalized_rules -> max, finalized_rules + default (where processed is nodata)
+    finalized_rules = merge_arrays([open_rasterio(x, cache=False) for x in finalized_rules], method='max')
 
-    processed_rules.data = np.where(processed_rules == processed_rules.rio.nodata, default_rules, processed_rules)
-    write_compress(processed_rules, save_dir / 'result.tif')
+    finalized_rules.data = np.where(finalized_rules == finalized_rules.rio.nodata, base_raster, finalized_rules)
+    write_compress(finalized_rules, save_dir / f'result_res_{resolution}_all_touched_{all_touched}.tif')
