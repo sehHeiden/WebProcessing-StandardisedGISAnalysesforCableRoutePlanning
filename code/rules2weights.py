ae00512a65fe06575849de7483fafa15967601ba
Sebastian Heiden <heiden-sebastian@t-online.de>
2022-11-19 17:02:41 +0100

    add requirements
    add comments to rules2weights.py

--- code/rules2weights.py
+++ code/rules2weights.py
@@ -18,12 +18,40 @@ from xarray import DataArray
 
 
 def write_compress(r: DataArray, save: Path) -> None:
+    """
+    Saving the raster r at save
+    :param r: raster as DataArray
+    :param save: save Path
+    :return: None (write)
+    """
     predict = 3 if np.issubdtype(r.dtype, np.floating) else 2
     r.rio.to_raster(save, driver='GTiff', tiled=True, compress='ZSTD',
                     num_threads='ALL_CPUS', predictor=predict, zstd_level=1)
 
 
+def write_png(r: DataArray, save: Path) -> None:
+    """
+    saving the raster r at save as png (gray)
+    :param r: raser as DataArray
+    :param save: save Path
+    :return: None (write)
+    """
+    # TODO: gray to pseudo colour: RdYlGn
+    lowest_value = r.data.min()
+    r.data = r.data / lowest_value
+    r = r.astype('uint16')
+    r.rio.to_raster(save, driver='PNG')
+
+
 def __filter(total_gdf: GeoDataFrame, column: str, _rule: Node, ) -> GeoDataFrame:
+    """
+    filtering the values of the GeoDataFrames for the attributes in the `column` of the GeoDataFrames,
+     that follow the `_rule`
+    :param total_gdf: original GeoDataFrame
+    :param column: column name (str) from which the attributes will be used
+    :param _rule: rule (as Node) that applies for the values in the column of the GeoDataFrame
+    :return: GeoDataFrame
+    """
     child_results: list[GeoDataFrame] = []
     for item in _rule.items:
         if isinstance(item, RoundNode):
@@ -57,13 +85,27 @@ def __filter(total_gdf: GeoDataFrame, column: str, _rule: Node, ) -> GeoDataFram
         return child_result
 
 
-def filter_gdf(_gdf: GeoDataFrame, column: str, values: str):
+def filter_gdf(_gdf: GeoDataFrame, column: str, values: str) -> GeoDataFrame:
+    """
+    filtering the values of the GeoDataFrames for the attributes in the `column` of the GeoDataFrames,
+     that follow the rule in `values`
+    :param _gdf: GeoDataFrame
+    :param column: name of the column (str)
+    :param values: str of the rules
+    :return: GeoDataFrame
+    """
     # decompose the value
     start_node = Node(values)
     return __filter(_gdf, column, start_node)
 
 
 def buffer(_gdf: GeoDataFrame, distance: float | str) -> GeoDataFrame:
+    """
+    Buffering the objects in GeoDataFrame by the value in `distance` or the rule in `distance` if it's a string
+    :param _gdf: GeoDataFrame
+    :param distance: float or str
+    :return: GeoDataFrame
+    """
     _gdf = _gdf.copy()
 
     if isinstance(distance, str):
@@ -71,11 +113,19 @@ def buffer(_gdf: GeoDataFrame, distance: float | str) -> GeoDataFrame:
     else:
         distance = distance if not np.isnan(distance) else 0
 
+    # TODO ray multi processing
     _gdf['geometry'] = _gdf['geometry'].buffer(distance)
     return _gdf
 
 
 def __estimate_buffer_size(data: GeoDataFrame, _buffer: Series, _rule: Node, ) -> Series:
+    """
+    estimate the size of the Buffer for every GeoObject in the GeoDataFrame
+    :param data: GeoDataFrame
+    :param _buffer: Series with the current buffer distances
+    :param _rule: rule to estimate the size of the buffer as Node
+    :return: Series
+    """
     child_results: list[Series | np.ndarray] = []
     for item in _rule.items:
         if isinstance(item, RoundNode):
@@ -96,6 +146,11 @@ def __estimate_buffer_size(data: GeoDataFrame, _buffer: Series, _rule: Node, ) -
 
 
 def get_command(_rule):
+    """
+    split the current `_rule` into an operator (faction) and its identifiers (parameters)
+    :param _rule: Node or a subtype?
+    :return: str, list[str]
+    """
     command = str(_rule.items[0]).split(' ')
     command = [x.strip() for x in command]
     operator = command[0]
@@ -109,6 +164,12 @@ def get_command(_rule):
 
 
 def make_processable(_df: DataFrame, main_path: Path) -> DataFrame:
+    """
+    change the DataFrame, so that it can be used to further process the rules it holdes
+    :param _df: DataFrame
+    :param main_path: Path of the main folder from the input data
+    :return: DataFrame
+    """
     _df = _df.copy()
 
     # drop Lines not usable (Use == NO), or lines without a path (dir AND FileName)
@@ -123,6 +184,13 @@ def make_processable(_df: DataFrame, main_path: Path) -> DataFrame:
 
 
 def __vector_processing(named_tuple, _crs: int, base_extent: tuple | None = None) -> GeoDataFrame:
+    """
+    Process the one single line of the rules DataFrame and return the processed GeoDataFrame
+    :param named_tuple: NamedTuple
+    :param _crs: EPSG-Code as int
+    :param base_extent: optional tuple
+    :return: GeoDataFrame
+    """
     layer_name = named_tuple.Layer if isinstance(named_tuple.Layer, str) else None
     gdf: GeoDataFrame = read_file(named_tuple.Path, bbox=base_extent, layer=layer_name)
     gdf = gdf.to_crs(epsg=_crs)
@@ -133,29 +201,55 @@ def __vector_processing(named_tuple, _crs: int, base_extent: tuple | None = None
     return buffer(gdf, named_tuple.Buffer)
 
 
-def process_base_rule(df: DataFrame, _resolution: float | tuple[float, list], _crs: int,
+def process_base_rule(df: DataFrame, _res: float | tuple[float, float], _crs: int,
                       _save_dir: Path, _all_touched: bool) -> DataArray:
-    named_tuple = list((df.copy().loc[df['Description'] == 'Base']).itertuples())[0]
-    vec = __vector_processing(named_tuple, _crs)
-    save_path = _save_dir / f"rule_{named_tuple.LineNum}_{named_tuple.Description}_res_{resolution}_all_touched_{all_touched}.tif"
-    raster = make_geocube(vec, ['Weights', ], resolution=resolution, fill=999,
+    """
+    Process the base rule of the rules DataFrame for the resolution `_res` and the `_crs`
+    in a `_all_touched` fashion or not.
+    and save the raster
+    at `_save_dir`
+    :param df: DataFrame with all rules
+    :param _res: float or tuple of two floats of the resolution
+    :param _crs: the CRS as EPSG-Code
+    :param _save_dir: save path of the raster
+    :param _all_touched: bool
+    :return: raster as DataArray
+    """
+    nmd_tpl = df.copy().loc[df['Description'] == 'Base'].iloc[0]
+    vec = __vector_processing(nmd_tpl, _crs)
+    save_path = _save_dir / f"rule_{nmd_tpl.LineNum}_{nmd_tpl.Description}_res_{_res}_all_touched_{_all_touched}.tif"
+    raster = make_geocube(vec, ['Weights', ], resolution=_res, fill=999,
                           rasterize_function=partial(rasterize_image, all_touched=_all_touched))
     raster = raster['Weights']
     write_compress(raster, save_path)
     return raster
 
 
-def process_rule(named_tuple, example_da: DataArray, _crs: int,
+def process_rule(nmd_tpl, example_da: DataArray, _crs: int,
                  _save_dir: Path, _all_touched: bool, bbox) -> list[str | Path]:
-    vec = __vector_processing(named_tuple, _crs, bbox)
+    """
+    Process the one single line of the rules DataFrame and return the save path in a list
+    (so that ic can be concat easily)
+    :param nmd_tpl: namedTuple
+    :param example_da: example raster as DataArray
+    :param _crs: crs as EPSG-Code
+    :param _save_dir: save dir of the raster
+    :param _all_touched: Bool process with all_touched?
+    :param bbox: bounding box, a tuple of 4 floats
+    :return: list[str| Path]
+    """
+    # TODO resolution from example_Data
+    res = int(abs(example_da.rio.transform()[0]))
 
-    save_path = _save_dir / f"res_{resolution}/all_touched_{all_touched}/rule_{named_tuple.LineNum}_{named_tuple.Description}.tif"
-    raster = make_geocube(vec, ['Weights'], like=example_da, fill=999,
-                          rasterize_function=partial(rasterize_image, all_touched=_all_touched))
-    raster = raster['Weights']
-    raster.data[example_da == example_da.rio.nodata] = raster.rio.nodata
-    save_path.parent.mkdir(parents=True, exist_ok=True)
-    write_compress(raster, save_path)
+    save_path = _save_dir / f"res_{res}/all_touched_{_all_touched}/rule_{nmd_tpl.LineNum}_{nmd_tpl.Description}.tif"
+    if not save_path.exists():  # reuse existing results
+        vec = __vector_processing(nmd_tpl, _crs, bbox)
+        raster = make_geocube(vec, ['Weights'], like=example_da, fill=999,
+                              rasterize_function=partial(rasterize_image, all_touched=_all_touched))
+        raster = raster['Weights']
+        raster.data[example_da == example_da.rio.nodata] = raster.rio.nodata
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        write_compress(raster, save_path)
     return [save_path, ]
 
 
@@ -165,9 +259,9 @@ if __name__ == '__main__':
     parser.add_argument('vectors_main_folder', type=str, help='Main Path to the vector files.')
     parser.add_argument('save_dir', type=str, help='Path where this projects raster files can the saved')
     parser.add_argument('-r', '--resolution', type=int, default=1000, help='Resolution to use.')
-    parser.add_argument('-at', '--all_touched', action='store_true',  help='Select those pixels, those CENTER  is '
-                                                                           'covered by the Polygone (False) or any '
-                                                                           'part overlaps with the Polygone (True).')
+    parser.add_argument('-at', '--all_touched', action='store_true', help='Select those pixels, those CENTER  is '
+                                                                          'covered by the Polygone (False) or any '
+                                                                          'part overlaps with the Polygone (True).')
     parser.add_argument('--crs', type=int, default=3157, help='Reference system. Default: Google Pseudo Mercator.')
 
     args = parser.parse_args()
@@ -190,9 +284,10 @@ if __name__ == '__main__':
         finalized_rules = finalized_rules + process_rule(rule, base_raster, crs, save_dir, all_touched,
                                                          base_raster.rio.bounds())
 
-    # combine the rasters file
+    # combine the raster's file
     # finalized_rules -> max, finalized_rules + default (where processed is nodata)
     finalized_rules = merge_arrays([open_rasterio(x, cache=False) for x in finalized_rules], method='max')
 
     finalized_rules.data = np.where(finalized_rules == finalized_rules.rio.nodata, base_raster, finalized_rules)
     write_compress(finalized_rules, save_dir / f'result_res_{resolution}_all_touched_{all_touched}.tif')
+    write_png(finalized_rules, save_dir / f'result_res_{resolution}_all_touched_{all_touched}.png')
