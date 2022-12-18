c3c4875692578f953415351b9eb6e0c58cc07568
Sebastian Heiden <heiden-sebastian@t-online.de>
2022-12-18 20:08:00 +0100

    add pywps_test.py  after renaming folder code to src. Try the LeastCostPath Process
     added line2line_distance.ipynb mentioned in last commit

---
+++ src/wps/pywps_test.py
@@ -0,0 +1,12 @@
+import flask
+import pywps
+
+from .processes.buffer import Buffer, MyBuffer
+from .processes.centroids import Centroids
+from .processes.least_cost_path import LeastCostPath
+
+
+service = pywps.Service([Buffer(), MyBuffer(), Centroids(), LeastCostPath()])
+app = flask.Flask(__name__)
+app.route('/wps', methods=['GET', 'POST'])(lambda: service)
+app.run()
