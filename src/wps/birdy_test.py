8142b8bb0779842fcb6a3c5df33184138b8b2cf0
Sebastian Heiden <tetje@yandex.ru>
2022-12-21 20:41:16 +0100

    pypwps_test.py  - added main pywps.cfg  - added allowedinputpaths
    birdy_test.py:
     - changed paths
     - print config of the Process
     - print the result of the service?

--- src/wps/birdy_test.py
+++ src/wps/birdy_test.py
@@ -2,17 +2,13 @@ from birdy import WPSClient
 from pathlib import Path
 
 pywps = WPSClient('http://localhost:5000/wps')
-line_path = Path(r'.\..\..\results\least_cost_paths\least_cost_path_test_points_res_10_al_false.gml')
-print(line_path.exists())
-line = open(line_path)
 
-cost_raster = Path(r".\..\..\results\weights\result_res_100_all_touched_True.tif")
-start_features = Path(r"..\..\results\test_points\start_point.gpkg")
-end_features = Path(r"..\..\results\test_points\end_point.gpkg")
+cost_raster = Path(r"./../../results/weights/result_res_100_all_touched_True.tif")
+start_features = Path(r"../../results/test_points/start_point.gpkg")
+end_features = Path(r"../../results/test_points/end_point.gpkg")
 
-pywps.lcp(costs=cost_raster,
-          start=start_features,
-          end=end_features).get(asobj=True)[0]
-
-# pywps.buffer(poly_in=line,
-#              buffer=10.0).get(asobj=True)[0]
+print(pywps.lcp.__doc__)
+result = pywps.lcp(costs=cost_raster,
+                   start=start_features,
+                   end=end_features)
+print(result.get(asobj=True)[0])  # print the geojson?
--- src/wps/pywps_test.py
+++ src/wps/pywps_test.py
@@ -5,8 +5,9 @@ from .processes.buffer import Buffer, MyBuffer
 from .processes.centroids import Centroids
 from .processes.least_cost_path import LeastCostPath
 
-
 service = pywps.Service([Buffer(), MyBuffer(), Centroids(), LeastCostPath()], ['pywps.cfg', ])
 app = flask.Flask(__name__)
 app.route('/wps', methods=['GET', 'POST'])(lambda: service)
-app.run()
+
+if __name__ == '__main__':
+    app.run()
