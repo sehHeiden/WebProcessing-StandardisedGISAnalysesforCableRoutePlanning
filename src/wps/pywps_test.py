a6d95eee8eec856d230f747db42bd65c5e577838
Sebastian Heiden <heiden-sebastian@t-online.de>
2022-12-21 20:08:25 +0100

    pywps_test.py pywps.cfg
    added config file and use for services

--- src/wps/pywps_test.py
+++ src/wps/pywps_test.py
@@ -6,7 +6,7 @@ from .processes.centroids import Centroids
 from .processes.least_cost_path import LeastCostPath
 
 
-service = pywps.Service([Buffer(), MyBuffer(), Centroids(), LeastCostPath()])
+service = pywps.Service([Buffer(), MyBuffer(), Centroids(), LeastCostPath()], ['pywps.cfg', ])
 app = flask.Flask(__name__)
 app.route('/wps', methods=['GET', 'POST'])(lambda: service)
 app.run()
