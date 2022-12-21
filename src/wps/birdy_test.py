a08a648f34d423e4e5fcd745807e48338261a7bd
Sebastian Heiden <tetje@yandex.ru>
2022-12-21 21:26:30 +0100

    added birdy_test.json pywps.cfg  - removed most allowedinputpaths
    birdy_test.py:
     - added ArgumentParser
     - added Json config

--- src/wps/birdy_test.py
+++ src/wps/birdy_test.py
@@ -1,14 +1,26 @@
 from birdy import WPSClient
 from pathlib import Path
+from json import load
+from argparse import ArgumentParser
 
-pywps = WPSClient('http://localhost:5000/wps')
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Find the nearest cost path.')
+    parser.add_argument('config_path', type=str, help='Full path to the config file. Containing relative paths.')
+    parser.add_argument('main_path', type=str, help='Main path for the files in the config.')
+    args = parser.parse_args()
 
-cost_raster = Path(r"./../../results/weights/result_res_100_all_touched_True.tif")
-start_features = Path(r"../../results/test_points/start_point.gpkg")
-end_features = Path(r"../../results/test_points/end_point.gpkg")
+    main_path = Path(args.main_path)
+    with open(args.config_path, 'r') as f:
+        config = load(f)
 
-print(pywps.lcp.__doc__)
-result = pywps.lcp(costs=cost_raster,
-                   start=start_features,
-                   end=end_features)
-print(result.get(asobj=True)[0])  # print the geojson?
+    pywps = WPSClient('http://localhost:5000/wps')
+
+    cost_raster = main_path / config['cost_raster']
+    start_features = main_path / config['start_features']
+    end_features = main_path / config['end_features']
+
+    print(pywps.lcp.__doc__)
+    result = pywps.lcp(costs=cost_raster,
+                       start=start_features,
+                       end=end_features)
+    print(result.get(asobj=True)[0])  # print the geojson?
