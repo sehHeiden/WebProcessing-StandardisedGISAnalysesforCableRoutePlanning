adddff1afe223c9ed88286ccdafc1690e35e0cbc
Sebastian Heiden <tetje@yandex.ru>
2022-12-23 20:58:17 +0100

    delete: ows_test.py
    renamed:
     - birdy_test.py -> birdy_flask.py and birdy_wsgi.py
     - birdy_test.json -> birdy.json
     - pywps.cfg -> pywps_flask.cfg and pywps_wsgi.cfg
     - pywps_test.py -> pywps_flask.py
    
     all double config are equal except for the different ports: 5000 for flask and 8081 for wsgi
    
     flask is working and wsgi not. The link of result.get()[0] is valid for flask and not valid for wsgi.

---
+++ src/wps/birdy_wsgi.py
@@ -0,0 +1,31 @@
+from birdy import WPSClient
+from pathlib import Path
+from json import load
+from argparse import ArgumentParser
+from metalink import download
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Find the nearest cost path with a wps using birdy.')
+    parser.add_argument('config_path', type=str, help='Full path to the config file. Containing relative paths.')
+    parser.add_argument('main_path', type=str, help='Main path for the files in the config.')
+    args = parser.parse_args()
+
+    main_path = Path(args.main_path)
+    with open(args.config_path, 'r') as f:
+        config = load(f)
+
+    pywps = WPSClient('http://localhost:8081/wps')
+
+    cost_raster = main_path / config['cost_raster']
+    start_features = main_path / config['start_features']
+    end_features = main_path / config['end_features']
+
+    result = pywps.lcp(costs=cost_raster,
+                       start=start_features,
+                       end=end_features)
+    print(result.get(asobj=True)[0])  # print the geojson?
+
+    f_name = str(result.get(asobj=False)[0])
+    print(f_name)
+    download.get(f_name, path='.', segmented=False)
